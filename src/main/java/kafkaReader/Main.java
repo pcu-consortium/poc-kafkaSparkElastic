d3b88944f587bf8b942c21301cb3901b2fd3e4c7
Smile-thest <thomas.estrabaud@smile.fr>
2017-06-22 15:36:28 +0200

    Ecriture dans elastic

--- src/main/java/kafkaReader/Main.java
+++ src/main/java/kafkaReader/Main.java
@@ -2,11 +2,16 @@ package kafkaReader;
 
 import java.util.regex.Pattern;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
+import org.elasticsearch.spark.sql.EsSparkSQL;
 
 public class Main {
 
@@ -19,12 +24,34 @@ public class Main {
 		JavaSparkContext jsc = new JavaSparkContext(sparkConf);
 		SparkSession ss = SparkSession.builder().getOrCreate();
 
+		Logger log = LogManager.getRootLogger();
+
 		// on lit le flux du kafka *EN BATCH*
 		Dataset<Row> ds1 = ss.read().format("kafka").option("kafka.bootstrap.servers", "localhost:9092")
-				.option("subscribe", "topic1").load().selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)",
+				.option("subscribe", "jsonToEs1").load().selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)",
 						"topic", "partition", "offset", "timestamp", "timestampType");
 
 		ds1.show(50, false);
-		// ds2.show(50, false);
+
+		// *****************************************************************
+		// ~~~~~~~Pour sauvegarder un message JSON qui vient de kafka~~~~~~~
+		// *****************************************************************
+
+		// C'est utilisable à partir du moment où on a tout l'objet JSON dans
+		// UNE COLONNE d'un dataframe
+
+		// On transforme le dataframe en RDD<Row> qu'on transforme en RDD de
+		// string
+		JavaRDD<String> sortieElasticJSON = ds1.select(ds1.col("value")).toJavaRDD().map(v1 -> v1.mkString());
+		JavaEsSpark.saveJsonToEs(sortieElasticJSON, "sparkDepuisKafka/test");
+
+		// *****************************************************************
+		// ~~~~~~~~~~~~~~~~~Pour sauvegarder une dataframe~~~~~~~~~~~~~~~~~~
+		// *****************************************************************
+
+		// On sauvegarde dans elastic
+		EsSparkSQL.saveToEs(ds1.select("value"), "sparkDepuisDataframe/test");
+		// JavaEsSpark.saveToEs(sortieElasticJSON, arg1);
 	}
+
 }
